8cb3f59b8085cc7e1deb31130f646018e6fe1032
Hinxcode <hinxcode@gmail.com>
2018-10-20 21:16:52 +0800

    update services

--- src/services/Chlor_a.js
+++ src/services/Chlor_a.js
@@ -1,17 +1,79 @@
 const path = require('path');
 const netcdf4 = require('netcdf4');
 
-const filePath = path.resolve(__dirname, '../assets/A2018001.L3m_DAY_CHL_chlor_a_4km.nc');
+const TARGET_VAR_KEY = 'chlor_a';
+const FILL_VALUE = -32767;
+const size = 50;
 
-const file = new netcdf4.File(filePath, 'r');
-const res = file.root.variables['chlor_a'].readSlice(0, 10, 1, 10);
+const filePath = path.resolve(__dirname, '../assets/A2018001.L3m_DAY_CHL_chlor_a_4km.nc');
+const source = new netcdf4.File(filePath, 'r');
+
+const getNearestLatValue = (arr, queryValue) => {
+  let result;
+
+  arr.forEach((value, index) => {
+    const floatQueryValue = Number.parseFloat(queryValue);
+  
+    if (floatQueryValue <= value && floatQueryValue >= arr[index + 1]) {
+      if (index + 1 < arr.length) {
+        result =  index + 1;
+      } else {
+        result =  index;
+      }
+
+      return;
+    }
+  });
+
+  return result;
+};
+
+const getNearestLonValue = (arr, queryValue) => {
+  let result;
+
+  arr.forEach((value, index) => {
+    const floatQueryValue = Number.parseFloat(queryValue);
+  
+    if (floatQueryValue >= value && floatQueryValue <= arr[index + 1]) {
+      if (index + 1 < arr.length) {
+        result =  index + 1;
+      } else {
+        result =  index;
+      }
+
+      return;
+    }
+  });
+
+  return result;
+};
 
 class Chlor_a {
   constructor() {
+    this.lats = source.root.variables['lat'].readSlice(0, 4320);
+    this.lons = source.root.variables['lon'].readSlice(0, 8640);
   }
 
   async find(params) {
-    return res;
+    const { query } = params;
+
+    const latIndex = getNearestLatValue(this.lats, query.lat);
+    const lonIndex = getNearestLonValue(this.lons, query.lon);
+
+    if (!latIndex || !lonIndex) return [];
+
+    const target = source.root.variables[TARGET_VAR_KEY].readSlice(latIndex, size, lonIndex, size);
+
+    return Object.values(target).reduce((arr, value, index) => {
+      const lat = this.lats[Math.floor(index / size) + Number.parseInt(latIndex)];
+      const lon = this.lons[index % size + Number.parseInt(lonIndex)];
+      
+      return value === FILL_VALUE ? arr : arr.concat(({
+        res: 4,
+        pos: { lat, lon },
+        val: value,
+      }));
+    }, []);
   }
 
   async get(id, params) {
@@ -21,7 +83,7 @@ class Chlor_a {
     //   throw new Error(`Message with id ${id} not found`);
     // }
 
-    // return message;
+    return {};
   }
 }
 
